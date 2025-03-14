715781ea0007e76b67ae89a4411b8567a395bb72
Mutlu Akgun <78053007+MutluRenegado@users.noreply.github.com>
2025-03-14 16:59:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
